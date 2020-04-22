e7e0cf8df1bb164d50ee9232aec2e002a13457ef
sujin <sujinemilycho@gmail.com>
2020-04-22 16:19:10 +0900

    feat: Spring MVC 실습

--- src/main/java/wooteco/helloworld/controller/UserController.java
+++ src/main/java/wooteco/helloworld/controller/UserController.java
@@ -1,38 +1,39 @@
 package wooteco.helloworld.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wooteco.helloworld.domain.User;
-
-import java.util.Map;
 
 @RestController
 public class UserController {
     /**
      * GET /users
      */
-    public String sayHi(String name) {
+    @GetMapping("/users")
+    public String sayHi(@RequestParam String name) {
         return "Hi " + name;
     }
 
     /**
      * GET /users/{id}
      */
-    public String retrieveUser(Long id) {
+    @RequestMapping("/users/{id}")
+    public String retrieveUser(@PathVariable Long id) {
         return "user's id is " + id;
     }
 
     /**
      * POST /users
      */
-    public String createUserWithJson(User user) {
+    @PostMapping("/users")
+    public String createUserWithJson(@RequestBody User user) {
         return "Hello " + user.getName();
     }
 
     /**
      * POST /users/form
      */
-    public String createUserWithForm(Map<String, String> paramMap) {
-        return "Hello " + paramMap.get("name");
+    @PostMapping("/users/form")
+    public String createUserWithForm(@ModelAttribute User user) {
+        return "Hello " + user.getName();
     }
-
 }
--- src/main/java/wooteco/helloworld/domain/User.java
+++ src/main/java/wooteco/helloworld/domain/User.java
@@ -30,4 +30,20 @@ public class User {
     public Integer getAge() {
         return age;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
