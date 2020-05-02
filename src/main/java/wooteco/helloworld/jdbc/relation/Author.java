2f66760ba695dc4f3f91e937f57c46108bb05641
sujin <sujinemilycho@gmail.com>
2020-05-02 16:56:29 +0900

    feat: 빠진 클래스 추가

---
+++ src/main/java/wooteco/helloworld/jdbc/relation/Author.java
@@ -0,0 +1,25 @@
+package wooteco.helloworld.jdbc.relation;
+
+import org.springframework.data.annotation.Id;
+
+public class Author {
+    @Id
+    private Long id;
+    private String name;
+
+    public Author() {
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
